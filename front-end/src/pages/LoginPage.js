124fb1757726885f8be11018925a44fd1e083e84
Kent Campbell <kentmcamp87@gmail.com>
2024-12-16 16:55:54 -0400

    initial commit

---
+++ front-end/src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+export const LogInPage = () => {
+    return (
+        <div className="content-container">
+            <h1>Log In</h1>
+            <input placeholder="email@domain.com" />
+            <input type="password" placeholder="password" />
+            <button>Log In</button>
+            <button>Forgot Password?</button>
+            <button>Sign Up</button>
+        </div>
+    )
+}
